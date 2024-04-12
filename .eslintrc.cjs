a454ee01e4c78f35a49f34e8424ff3ac9238d249
jerryChas <jerry.olsson@chasacademy.se>
2024-04-12 19:37:27 +0200

    setup prettier workspace

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -18,4 +19,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
