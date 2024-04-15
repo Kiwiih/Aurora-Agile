6405dc27d4f83e3ea55ee592fb35e4d52aeb6d6b
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-04-15 15:57:04 +0200

    Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './assets/auroraCustomStyles.css';
+
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 
