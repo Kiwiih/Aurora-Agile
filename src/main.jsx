886957eecfc4b3158d63f10cf6337de4abace6b3
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-04-15 08:05:08 +0200

    bootstrap and poppy is installed

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+
+// Bootstrap:
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Aurora custom theme:
+import './assets/styles/auroraColorTheme.css';
+
 import { Provider } from 'react-redux';
 import { store } from './store/store.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+
 export const store = configureStore({
-  reducer: '',
+    reducer: '',
 });
