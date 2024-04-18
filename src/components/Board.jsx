96dc5a7fb326efe028bb8da8ab1356ae2a7a1205
Moa Sandberg <moisan1999@gmail.com>
2024-04-18 11:17:20 +0200

    started constructing the board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { columnSlice } from '../features/column/columnSlice';
+
+//Components
+// import Header from './Header';
+import Column from './Column';
+import CreateNewTask from './CreateNewTask';
 
 const Board = () => {
-  return <div>Board</div>;
+  const [user, setUser] = useState([
+    'all',
+    'Moa',
+    'Alicia',
+    'Emil',
+    'Paulina',
+    'Viktor',
+    'Jerry',
+  ]);
+
+  const columns = useSelector((state) => state[columnSlice.name].columns);
+  console.log(columns);
+  return (
+    <>
+      {/* <Header
+        user={user}
+        setUser={setUser}
+      /> */}
+      <CreateNewTask />
+      {columns.map((column) => (
+        <Column
+          columnId={column.id}
+          user={user}
+        />
+      ))}
+    </>
+  );
 };
 
 export default Board;
