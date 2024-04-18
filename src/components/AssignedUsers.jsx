fb278255382ec8dfe7b5f83ed803a7de2577fea9
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-04-18 11:08:24 +0200

    Created the circles for assigned users not finished

---
+++ src/components/AssignedUsers.jsx
@@ -0,0 +1,14 @@
+
+
+const AssignedUsers = () => {
+  return (
+    <div className="assigned">
+    <div className="rounded-circle bg-aurora-secondary opacity-100 circle">AB</div>
+    <div className="rounded-circle bg-aurora-secondary opacity-75 circle">CD</div>
+    <div className="rounded-circle bg-aurora-secondary opacity-50 circle">EF</div>
+    <div className="rounded-circle circle bg-transparent fs-3">+</div>
+    </div>
+  )
+}
+
+export default AssignedUsers;
