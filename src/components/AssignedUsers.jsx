db8170b161f564a342961dffb424114981433455
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-04-19 15:30:25 +0200

    added some functionality to assignedUsers but far from finished

--- src/components/AssignedUsers.jsx
+++ src/components/AssignedUsers.jsx
@@ -1,31 +1,37 @@
 import {useSelector, useDispatch} from 'react-redux';
-import { taskSlice } from '../features/task/taskSlice';
+import { editTask } from '../features/task/taskSlice';
 
 const AssignedUsers = () => {
 
-// const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const tasks = useSelector(state => state.tasks);
+  // console.log(tasks);
 
-// const assignedTo = useSelector(state => state.task.tasks[0].assignedTo);
+  const taskAssigned = tasks.map(task => task.assignedTo);
+  console.log(taskAssigned);
 
-// const handleAddUser = () => {
-//     const taskId = 1;
-//     const newUser = "XX";
-    
-//     const updatedTask = {
-//       id: taskId,
-//       assignedTo: [...assignedTo, newUser]
-//     };
-
-//     dispatch(editTask(updatedTask));
-//   };
+  const handleAddUser = (taskId, userId) => {
+    /* denna funktion kommer att göras om så att en dropdown öppnas
+    vid klick på plus. så att man kan välja VEM som ska läggas till. */
+    console.log("användare skall läggas till")
+    const task = tasks.find(task => task.id === taskId);
+    if (task) {
+      const updatedAssignedTo = [...task.assignedTo, userId];
+      dispatch(editTask({ taskId, assignedTo: updatedAssignedTo }));
+    }
+  }
 
   return (
     <div className="assigned">
+    {/* saknas conditional rendering som jag kommer lägga till senare: 
+    första cirkeln syns vid 1 assignedUser, andra vid 2, tredje vid 3. 
+    fler än 3 kommer ej synas. ev punkter vid fler än 3 users.
+    Plusset ska enbart synas i modalen, ej i card.
+    */}
     <div className="rounded-circle bg-aurora-secondary opacity-100 circle">AB</div>
     <div className="rounded-circle bg-aurora-secondary opacity-75 circle">CD</div>
     <div className="rounded-circle bg-aurora-secondary opacity-50 circle">EF</div>
-    <div className="rounded-circle circle bg-transparent fs-3">+</div>
-    {/* plusset ska ha onClick={handleAddUser} typ */}
+    <div className="rounded-circle circle bg-transparent fs-3" onClick={handleAddUser}>+</div>
     </div>
   )
 }
--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -14,17 +14,23 @@ const initialState = {
   ],
 };
 
-export const taskSlice = createSlice({
-  name: 'task',
-  initialState,
-  reducers: {
-    moveTask: () => {},
-    addTask: () => {},
-    removeTask: () => {},
-    editTask: () => {},
-  },
-});
+  export const taskSlice = createSlice({
+    name: 'task',
+    initialState,
+    reducers: {
+      moveTask: () => {},
+      addTask: () => {},
+      removeTask: () => {},
+      editTask: (state, action) => {
+        const { taskId, assignedTo } = action.payload;
+        const taskToUpdate = state.tasks.find(task => task.id === taskId);
+        if (taskToUpdate) {
+          taskToUpdate.assignedTo = assignedTo;
+        }
+      },
+    },
+  });
 
-export const {} = taskSlice.actions;
+export const { editTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
