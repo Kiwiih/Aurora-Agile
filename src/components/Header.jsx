7c29dfd4dc7e51b929210003a7653f4d671024fe
Alicia <aliciaxd@live.se>
2024-04-19 14:10:02 +0200

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,23 +3,38 @@ import { Dropdown } from 'react-bootstrap';
 const Header = ({setUser}) => {
 
   return (
-    <Dropdown>
-      <Dropdown.Toggle
-        variant='secondary'
-        id='dropdown-basic'
-      >
-        Select User
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <Dropdown.Item onClick={() => setUser('all')}>All users</Dropdown.Item>
-        <Dropdown.Item onClick={() => setUser('Moa')}>Moa</Dropdown.Item>
-        <Dropdown.Item onClick={() => setUser('Alicia')}>Alicia</Dropdown.Item>
-        <Dropdown.Item onClick={() => setUser('Paulina')}>Paulina</Dropdown.Item>
-        <Dropdown.Item onClick={() => setUser('Viktor')}>Viktor</Dropdown.Item>
-        <Dropdown.Item onClick={() => setUser('Jerry')}>Jerry</Dropdown.Item>
-        <Dropdown.Item onClick={() => setUser('Emil')}>Emil</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+    <header className='text-bg-aurora-light'>
+      <Dropdown >
+        <Dropdown.Toggle
+          className='btn btn-aurora-primary'
+          variant='secondary'
+          id='dropdown-basic'
+        >
+          Select User
+        </Dropdown.Toggle>
+        <Dropdown.Menu className='btn btn-aurora-secondary'>
+          <Dropdown.Item onClick={() => setUser('all')}>
+            All users
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setUser('Moa')}>Moa</Dropdown.Item>
+          <Dropdown.Item onClick={() => setUser('Alicia')}>
+            Alicia
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setUser('Paulina')}>
+            Paulina
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setUser('Viktor')}>
+            Viktor
+          </Dropdown.Item>
+          <Dropdown.Item onClick={() => setUser('Jerry')}>Jerry</Dropdown.Item>
+          <Dropdown.Item onClick={() => setUser('Emil')}>Emil</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <img
+        src='../src/assets/logos/LogoSmall/AuroraAgileOriginalLogoColorSmall.png'
+        alt='Aurora Agile Logo in blue and green colors'
+      />
+    </header>
   );
 }
 
