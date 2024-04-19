d8b894f17c0f59786ceb32b46ce0c776619b73bf
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-04-19 16:57:44 +0200

    commit

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { columnSlice } from '../features/column/columnSlice';
-
 //Components
 import Header from './Header';
 import Column from './Column';
 import CreateNewTask from './CreateNewTask';
-
 const Board = () => {
   const [user, setUser] = useState([
     'all',
@@ -17,30 +14,33 @@ const Board = () => {
     'Viktor',
     'Jerry',
   ]);
-
   const columns = useSelector((state) => state[columnSlice.name].columns);
   console.log(columns);
   return (
     <>
-      <Header
-        setUser={setUser}
-      /> 
-      <main>
-        <div className='createNewTask_section'>
-          <CreateNewTask />
-        </div>
-        <div className='column_section'>
-          {columns.map((column) => (
-            <Column
-              key={column.id}
-              columnId={column.id}
-              user={user}
-            />
-          ))}
-        </div>
-      </main>
+      <div className='bodyContainer'>
+
+
+        <Header
+          setUser={setUser}
+        />
+        <main>
+          <div className='createNewTask_section'>
+            <CreateNewTask />
+          </div>
+          <div className='column_section'>
+            {columns.map((column) => (
+              <Column
+                key={column.id}
+                columnId={column.id}
+                user={user}
+              />
+            ))}
+          </div>
+        </main>
+
+      </div>
     </>
   );
 };
-
 export default Board;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,37 +1,74 @@
-// Task
-
-import React from 'react';
-
-const task = ({ title, description, doDate, assignedTo, deadline }) => {
-  return (
-    <div
-      className='card p-2  bg-gradient shadow border-0  '
-      style={{ width: '300px' }}
-    >
-      <div className='d-flex justify-content-between'>
-        <h3 className='h6 text-truncate'>{title}</h3>
-
-        <p
-          style={{ fontSize: '80%' }}
-          className='m-0 flex-shrink-0'
-        >
-          Do Date: <br />
-          {doDate}
-        </p>
-      </div>
-
-      <div className='d-flex justify-content-between'>
-        <p className='m-0'>{assignedTo}</p>
-        <p
-          style={{ fontSize: '80%' }}
-          className='m-0'
-        >
-          Deadline: <br />
-          {deadline}
-        </p>
-      </div>
-    </div>
-  );
+// taskSlice
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+const initialState = {
+  tasks: [
+    {
+      id: '1',
+      title: 'Implementera användarautentisering',
+      description:
+        'Implementera användarautentisering med hjälp av JWT och Express-session.',
+      deadline: '2024-05-10',
+      doDate: '2024-04-20',
+      assignedTo: [1],
+      columnId: 1,
+    },
+    {
+      id: '2',
+      title: 'Designa användargränssnitt',
+      description:
+        'Skapa användargränssnitt för webbapplikationen med hjälp av React och Material-UI.',
+      deadline: '2024-05-15',
+      doDate: '2024-04-22',
+      assignedTo: [2, 3],
+      columnId: 2,
+    },
+    {
+      id: '3',
+      title: 'Optimera databasförfrågningar',
+      description:
+        'Optimera databasförfrågningar för att förbättra prestandan hos webbapplikationen.',
+      deadline: '2024-05-20',
+      doDate: '2024-04-25',
+      assignedTo: [4],
+      columnId: 3,
+    },
+    {
+      id: '4',
+      title: 'Uppdatera dokumentation',
+      description:
+        'Uppdatera dokumentation för att återspegla de senaste ändringarna i koden.',
+      deadline: '2024-05-12',
+      doDate: '2024-04-28',
+      assignedTo: [5],
+      columnId: 1,
+    },
+    {
+      id: '5',
+      title: 'Testa gränssnittsfunktionalitet',
+      description:
+        'Utför enhetstester och integrationstester för att säkerställa gränssnittsfunktionalitetens korrekthet.',
+      deadline: '2024-05-18',
+      doDate: '2024-04-30',
+      assignedTo: [6, 5],
+      columnId: 1,
+    },
+  ],
 };
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      const newTask = {
+        id: nanoid(),
+        title: action.payload,
+      };
+    },
+    removeTask: (state, action) => { },
+    moveTask: (state, action) => { },
+    editTask: (state, action) => { },
+  },
+});
 
-export default task;
+export const { moveTask, addTask, removeTask, editTask } = taskSlice.actions;
+export default taskSlice.reducer;
