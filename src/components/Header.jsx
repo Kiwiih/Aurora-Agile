c3eb2f3cf60d48c8715b6f38962c3c2d43fb0c56
Alicia <aliciaxd@live.se>
2024-04-25 09:30:31 +0200

    active users is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 import { Dropdown } from 'react-bootstrap';
 import auroraAgileLogo from '../assets/logos/LogoSmall/AuroraAgileOriginalLogoColorSmall.png';
+import { useState } from 'react';
 
 const Header = ({ setUser }) => {
+  const [activeUser, setActiveUser] = useState(null);
+
+  const handleUser = (userId) => {
+    setUser(userId)
+    setActiveUser(userId)     
+  }
+
+  console.log(setUser, activeUser)
+
+
   return (
     <header className='text-bg-aurora-dark'>
       <Dropdown>
@@ -13,13 +24,62 @@ const Header = ({ setUser }) => {
           Select User
         </Dropdown.Toggle>
         <Dropdown.Menu className='btn btn-aurora-secondary'>
-          <Dropdown.Item onClick={() => setUser(null)}>All users</Dropdown.Item>
-          <Dropdown.Item onClick={() => setUser(1)}>Moa</Dropdown.Item>
-          <Dropdown.Item onClick={() => setUser(2)}>Alicia</Dropdown.Item>
-          <Dropdown.Item onClick={() => setUser(3)}>Paulina</Dropdown.Item>
-          <Dropdown.Item onClick={() => setUser(4)}>Viktor</Dropdown.Item>
-          <Dropdown.Item onClick={() => setUser(5)}>Jerry</Dropdown.Item>
-          <Dropdown.Item onClick={() => setUser(6)}>Emil</Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(null)}
+            style={{
+              backgroundColor: activeUser === null ? '#ffffff' : 'initial',
+            }}
+          >
+            All users
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(1)}
+            style={{
+              backgroundColor: activeUser === 1 ? '#ffffff' : 'initial',
+            }}
+          >
+            Moa
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(2)}
+            style={{
+              backgroundColor: activeUser === 2 ? '#ffffff' : 'initial',
+            }}
+          >
+            Alicia
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(3)}
+            style={{
+              backgroundColor: activeUser === 3 ? '#ffffff' : 'initial',
+            }}
+          >
+            Paulina
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(4)}
+            style={{
+              backgroundColor: activeUser === 4 ? '#ffffff' : 'initial',
+            }}
+          >
+            Viktor
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(5)}
+            style={{
+              backgroundColor: activeUser === 5 ? '#ffffff' : 'initial',
+            }}
+          >
+            Jerry
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => handleUser(6)}
+            style={{
+              backgroundColor: activeUser === 6 ? '#ffffff' : 'initial',
+            }}
+          >
+            Emil
+          </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
       <img
