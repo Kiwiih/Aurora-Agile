fe8877435e173f8c9c1145fead62645b87d789b2
Alicia <aliciaxd@live.se>
2024-04-26 12:00:30 +0200

    routing, missingpage, links

---
+++ src/components/MissingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TrasigRobot from '../assets/trasigrobot.png'
+
+const MissingPage = () => {
+  return (
+    <main>
+    <img style={{flexGrow: "1"}} src={TrasigRobot} alt="Broken Robot" />
+</main>
+  )
+}
+
+export default MissingPage
