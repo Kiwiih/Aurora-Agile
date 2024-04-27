a081b31f22d20719a842b338a2d2712e39648777
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-04-27 20:09:23 +0200

    dot dot dot exist

--- src/components/AssignedUsers.jsx
+++ src/components/AssignedUsers.jsx
@@ -4,36 +4,34 @@ import { useState } from 'react';
 import { userSlice } from '../features/user/userSlice';
 
 const AssignedUsers = ({ taskId, user }) => {
-
   const dispatch = useDispatch();
-  const tasks = useSelector(state => state.task.tasks) || [];
-  const users = useSelector(state => state.user.users);
+  const tasks = useSelector((state) => state.task.tasks) || [];
+  const users = useSelector((state) => state.user.users);
   // console.log(users);
   // console.log(tasks);
 
   const [showDropdown, setShowDropdown] = useState(false);
 
-// om user är 1 så vill vi bara ha de tasks där assignedTo innehåller 1. includes()
+  // om user är 1 så vill vi bara ha de tasks där assignedTo innehåller 1. includes()
 
-    let currentTask;
-    
-    if (user === null) { currentTask = tasks.find((task) => {
-    return task.id === taskId;
-  })
-    }
+  let currentTask;
 
+  if (user === null) {
+    currentTask = tasks.find((task) => {
+      return task.id === taskId;
+    });
+  }
 
-    if (user !== null) { 
-      tasks.forEach(t => {
-        if (t.assignedTo.includes(user)) {
-          currentTask = t;
-        }
-      });
+  if (user !== null) {
+    tasks.forEach((t) => {
+      if (t.assignedTo.includes(user)) {
+        currentTask = t;
+      }
+    });
     //   currentTask = tasks.find((task) => {
     //   return task.id === taskId;
     // })
-      }
-
+  }
 
   const assignedTo = currentTask.assignedTo;
   // console.log(user)
@@ -48,46 +46,65 @@ const AssignedUsers = ({ taskId, user }) => {
     //   const updatedAssignedTo = [...task.assignedTo, userId];
     //   dispatch(editTask({ taskId, assignedTo: updatedAssignedTo }));
     // }
-  }
-
-
-
+  };
 
   return (
-    <div className="assigned">
-    {/*
+    <div className='assigned'>
+      {/*
     TODO: Plusset ska enbart synas i modalen, ej i card. - eller? fixa senare isf.
     */}
 
-
-
-
-{/* assigned to är vilka som är assigned på aktuellt task.
+      {/* assigned to är vilka som är assigned på aktuellt task.
   den visar max 3 cirklar. den visar första bokstaven i för- och efternamn.
   vid mer än ett efternamn syns bara första. */}
-{assignedTo.map((person, index) => (
-  index < 3 && (
-    <div key={index} className={`rounded-circle bg-aurora-secondary opacity-${100 - (index * 25)} circle`}>
-      {assignedTo.length > 0 && <span key={index}>{users.find((u) => u.id === person).name.split(" ").map((name, i) => i < 2 && name.charAt(0))}</span>}
+      {assignedTo.map(
+        (person, index) =>
+          index < 3 && (
+            <div
+              key={index}
+              className={`me-1 rounded-circle text-bg-aurora-secondary opacity-${100 - index * 25} circle`}
+            >
+              {assignedTo.length > 0 && (
+                <span key={index}>
+                  {users
+                    .find((u) => u.id === person)
+                    .name.split(' ')
+                    .map((name, i) => i < 2 && name.charAt(0))}
+                </span>
+              )}
+            </div>
+          )
+      )}
+      {/* om det finns fler än 3 assignade till uppgiften så vissas (...) */}
+      {assignedTo.length > 3 && (
+        <div className='rounded-circle border bg-light circle'>...</div>
+      )}
+
+      <div
+        className='rounded-circle circle bg-transparent fs-3'
+        onClick={handleAddUser}
+      >
+        +
+      </div>
+
+      {/* implementera senare att bara de som inte redan är assignade till tasket syns som options nedan */}
+      {showDropdown && (
+        <select
+          name='users'
+          id='users'
+        >
+          {users.map((user) => (
+            <option
+              key={user.id}
+              value={user.id}
+            >
+              {user.name}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
-    )
-  
-))}
-
-    <div className="rounded-circle circle bg-transparent fs-3" onClick={handleAddUser}>+</div>
-
-
-    {/* implementera senare att bara de som inte redan är assignade till tasket syns som options nedan */}
-    {showDropdown && 
-  <select name="users" id="users">
-        {users.map((user) => (
-          <option key={user.id} value={user.id}>{user.name}</option>
-        ))}
-  </select>}
-    </div>
-  )
-}
-
-
+  );
+};
 
 export default AssignedUsers;
